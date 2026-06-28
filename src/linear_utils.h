69224fee68c91471e77bbad415f8471ef6409c62
Drake2034 <kaissous.hf@gmail.com>
2026-06-28 15:08:39 +0100

    add matrix initialization and priting methods

---
+++ src/linear_utils.h
@@ -0,0 +1,5 @@
+#ifndef LINEAR_UTILS_H
+#define LINEAR_UTILS_H
+
+    
+#endif
---
+++ tests/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "linear/linear.h"
+
+int main(){
+    scalar_t scalar = 12.0f;
+    printf("%f\n", scalar);
+    return 0;
+}
